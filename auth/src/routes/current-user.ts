edb5039526364f446dde74db34a469ee4fb52ea2
Rob Thorne <rob@torenware.com>
2021-04-20 18:56:34 -0700

    Moved current user handling to middleware

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,21 +1,12 @@
 import express from 'express';
-import jwt from 'jsonwebtoken';
+
+import { currentUser } from '../middlewares/current-user';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req, res) => {
-  // Note the ? which makes TS check iff session exists.
-  if (!req.session?.jwt) {
-    return res.send({ currentUser: null });
-  }
-
-  // apparently logged in, but make sure we are not being hacked.
-  try {
-    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
-    return res.send({ currentUser: payload });
-  } catch (err) {
-    return res.send({ currentUser: null });
-  }
+router.get('/api/users/currentuser', currentUser, (req, res) => {
+  // Out middleware has already set up the currentUser
+  res.send({ currentUser: req.currentUser || null });
 });
 
 export { router as currentUserRouter };
