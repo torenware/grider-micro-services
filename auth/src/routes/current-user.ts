95856fce7d26341500376aecb738ef3aa3aa85b2
Rob Thorne <rob@torenware.com>
2021-04-20 18:18:05 -0700

    implementing current-user and signout

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,21 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
 router.get('/api/users/currentuser', (req, res) => {
-  res.send('Hi there woild!');
+  // Note the ? which makes TS check iff session exists.
+  if (!req.session?.jwt) {
+    return res.send({ currentUser: null });
+  }
+
+  // apparently logged in, but make sure we are not being hacked.
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    return res.send({ currentUser: payload });
+  } catch (err) {
+    return res.send({ currentUser: null });
+  }
 });
 
 export { router as currentUserRouter };
