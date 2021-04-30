b0afdcbc67b896cc2787016999ecbb3a6539bb58
Rob Thorne <rob@torenware.com>
2021-04-30 12:25:51 -0700

    Upgrade common to 1.0.11

--- update-common.sh
+++ update-common.sh
@@ -1,9 +1,11 @@
 #! /usr/local/bin/bash
 
-DIRS="auth tickets orders nats-test"
+DIRS="auth tickets orders nats-test expiration"
 
 for proj in $DIRS; do
+  echo "-----------------"
   echo $proj
+  echo "-----------------"
   pushd $proj
   yarn upgrade @grider-courses/common --latest
   popd
