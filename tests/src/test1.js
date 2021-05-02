e02bd9a0fc7cfb1997fc70e3c81d75a8f5d4a666
Rob Thorne <rob@torenware.com>
2021-05-01 17:03:48 -0700

    Get script to create an order

--- tests/src/test1.js
+++ tests/src/test1.js
@@ -2,6 +2,23 @@ import axios from 'axios';
 import https from 'https';
 import fs, { promises } from 'fs';
 
+const options = {
+  httpsAgent: new https.Agent({
+    rejectUnauthorized: false
+  }),
+  timeout: 10 * 1000
+};
+
+const loadOptions = () => {
+  const cookie = loadCookie();
+  const optionsWithCookie = { ...options };
+  if (cookie) {
+    optionsWithCookie['headers'] = {
+      cookie
+    };
+  }
+  return optionsWithCookie;
+};
 
 
 const writeCookie = async (data) => {
@@ -11,49 +28,99 @@ const writeCookie = async (data) => {
 };
 
 const loadCookie = () => {
-  const cookies = [];
   let data;
   try {
+    if (!fs.existsSync('./cookies.txt')) {
+      console.log('No cookie file found.')
+      return null;
+    }
     data = fs.readFileSync('./cookies.txt');
-    console.log('read it maybe');
-    cookies.push(data);
-    return cookies;
+    return data;
   }
   catch (err) {
     console.log('loadCookie:', err.toString());
   }
+  return null;
 };
 
-const options = {
-  httpsAgent: new https.Agent({
-    rejectUnauthorized: false
-  })
-};
 
-const doGet = async () => {
+const currentUser = async () => {
   const url = 'https://ticketing.local/api/users/currentuser';
+
   try {
-    const response = await axios.get(url, options);
-    console.log('we returned');
-    console.log(response.data, typeof response.data);
-    console.log(response.headers);
-    await writeCookie(response.headers['set-cookie'][0]);
+    const response = await axios.get(url, loadOptions());
+    console.log(response.data);
 
   }
   catch (err) {
+    console.log(err);
     console.log(err.toString());
 
   }
 };
 
+const createTicket = async (title, price) => {
+  const data = {
+    title,
+    price: parseFloat(price)
+  }
+  const url = 'https://ticketing.local/api/tickets';
+  try {
+    const response = await axios.post(url, data, loadOptions());
+    // axios does not implement await correctly; you *always*
+    // get back a Promise<Pending>. This sucks. WTF?
+    return await response.data;
+  }
+  catch (err) {
+    console.error('threw in createTicket');
+    console.error(err.toString());
+  }
+  return {};
+};
+
+const createOrder = async (ticketId) => {
+  const data = {
+    ticketId
+  };
+  const url = 'https://ticketing.local/api/orders';
+  try {
+    const response = await axios.post(url, data, loadOptions());
+    // axios does not implement await correctly; you *always*
+    // get back a Promise<Pending>. This sucks. WTF?
+    return await response.data;
+  }
+  catch (err) {
+    console.error('threw in createTicket');
+    console.error(err.toString());
+  }
+  return {};
+};
+
 // https://gist.github.com/nzvtrk/ebf494441e36200312faf82ce89de9f2
-const login = async () => {
+const signUp = async (email, password) => {
   const url = 'https://ticketing.local/api/users/signup';
 
   try {
     const response = await axios.post(url, {
-      email: 'test7@email.com',
-      password: 'not-real-secure'
+      email,
+      password
+    }, options);
+
+    await writeCookie(response.headers['set-cookie'][0]);
+
+  }
+  catch (err) {
+    console.log(err.toString());
+  }
+};
+
+const signIn = async (email, password) => {
+  const url = 'https://ticketing.local/api/users/signin';
+
+  try {
+    const response = await axios.post(url, {
+      email,
+      password
     }, options);
 
     console.log(response.data);
@@ -66,6 +133,23 @@ const login = async () => {
   }
 };
 
-const cookie = loadCookie();
-console.log(cookie);
+const signOut = () => {
+  // Just toss the cookie.
+  if (fs.existsSync('./cookies.txt')) {
+    fs.unlinkSync('./cookies.txt');
+    console.log('deleted cookie file');
+  }
+}
+
+//signUp('yaya@yayas.org', 'yayayayaya');
+currentUser();
+const data = createTicket('Show That Never Ends', 42);
+data.then(rslt => {
+  console.log(rslt);
+  const ticketId = rslt.id;
+  const orderPromise = createOrder(ticketId);
+  orderPromise.then(order => {
+    console.log('Order:', order);
+  })
+});
 
