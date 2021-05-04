690935fdb09b5b9c1565ec0eafa0a4af009caf3f
Rob Thorne <rob@torenware.com>
2021-05-04 16:33:53 -0700

    fix some annoying warnings on nextjs build

--- client/hooks/use-request.js
+++ client/hooks/use-request.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { useState } from 'react';
 
-export default ({ url, method, body, onSuccess = null }) => {
+const useRequest = ({ url, method, body, onSuccess = null }) => {
   const [errors, setErrors] = useState(null);
 
   const doRequest = async (bodyParams = {}) => {
@@ -45,3 +45,5 @@ export default ({ url, method, body, onSuccess = null }) => {
 
   return { doRequest, errors };
 };
+
+export default useRequest;
