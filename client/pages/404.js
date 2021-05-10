1671c8e560e5fe0c2ca3c246f0565e7c3d9135f2
Rob Thorne <rob@torenware.com>
2021-05-10 11:06:55 -0700

    Header now appears on 404 page

--- client/pages/404.js
+++ client/pages/404.js
@@ -1,4 +1,25 @@
+import Header from '../components/header';
+
 export default function Custom404() {
-  return <div className='container'><h1>404 - Page Not Found</h1></div>;
+
+  return (
+    <div className='error'>
+      <div className='container'>
+        <h1>404 - Page Not Found</h1>
+      </div>
+    </div>
+  );
+
+
 }
+
+export async function getStaticProps(context) {
+  return {
+    props: {
+      noLinks: true
+    }
+  }
+
+}
+
 
