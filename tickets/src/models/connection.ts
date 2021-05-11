13512b5ee65584b6e51b9df8dac0df7d22f038e2
Rob Thorne <rob@torenware.com>
2021-05-10 19:43:20 -0700

    Removed references to the mongoose-auto-increment plugin the tests did not catch

--- tickets/src/models/connection.ts
+++ tickets/src/models/connection.ts
@@ -1,9 +1,4 @@
 import mongoose from 'mongoose';
-import { Ticket } from './ticket';
-
-// Old plugin for autoincrement.
-// tslint:disable-next-line: no-var-requires
-const autoIncrement = require('mongoose-auto-increment');
 
 export const buildConnection = async () => {
   const conn = await mongoose.connect(process.env.MONGO_URI!, {
@@ -11,12 +6,5 @@ export const buildConnection = async () => {
     useUnifiedTopology: true,
     useCreateIndex: true,
     useFindAndModify: false,
-  });
-
-  autoIncrement.initialize(conn);
-  Ticket.schema.plugin(autoIncrement.plugin, {
-    model: 'Ticket',
-    field: 'serial',
-    startAt: 1000,
   });
 };
