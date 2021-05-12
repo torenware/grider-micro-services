11144479d73a48236ec740c9672181ecfd033049
Rob Thorne <rob@torenware.com>
2021-05-11 23:36:35 -0700

    yet another trival change to test automation

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -24,7 +24,7 @@ const start = async () => {
     console.error(err);
   }
   app.listen(3000, () => {
-    console.log('Listening on port 3000!!!!!!!!');
+    console.log('Listening on port 3000 for auth!!!!!!!!');
   });
 };
 
